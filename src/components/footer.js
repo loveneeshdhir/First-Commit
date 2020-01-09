6be821e8bd318d3118adbea5de0d53858084db7b
Loveneesh Dhir <45602254+loveneeshdhir@users.noreply.github.com>
2020-01-09 23:35:30 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,9 @@ import Link from 'gatsby-link'
 const Footer = ({ author, profile }) => (
     <footer>
         Made with 
-        <a href="https://github.com/loveneeshdhir"> @lovneeshdir</a>
+        
         <i className="em em-heart" style={{width: '15px', height: '15px'}}></i>
+        <a href="https://github.com/loveneeshdhir"> @lovneeshdhir</a>
     </footer>
 )
 
